500e3d366054019eddb330b73e1109b85f977920
AlgoPrashant <prashant.verma@algorisys.com>
2024-09-12 18:36:24 +0530

    Initial commit

--- prisma/migrations/20240812060853_model_application_schema/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `imageUrl` on the `Application` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Application" DROP COLUMN "imageUrl",
-ADD COLUMN     "imagePath" TEXT;
